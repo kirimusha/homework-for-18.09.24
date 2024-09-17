88c0bf01ec091885ca9e9b06d31d767d5d84eaea
Maria <mariya.Kirichek2006@mail.ru>
2024-09-17 23:13:40 +0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int h, v, a, g = 9.8, x, y, t;
+    cout << "Enter h: ";
+    cin >> h;
+    cout << "Enter v: ";
+    cin >> v;
+    cout << "Enter a: ";
+    cin >> a;
+    t = v * sin(a) / g * (1 + sqrt(1 + (2 * g * h) / (pow(v, 2) * pow(sin(a), 2))));
+    x = v * t * cos(a);
+    y = h + v * t * sin(a) - 0.5 * g * pow(t, 2);
+    cout << "Result x: " << x << endl;
+    cout << "Result y: " << y << endl;
+    return 0;
+}
