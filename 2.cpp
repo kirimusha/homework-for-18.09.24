dca55df0eed1b1c53e50f2039a261405f84f0236
Maria <mariya.Kirichek2006@mail.ru>
2024-09-23 10:23:56 +0300

    Add files via upload

--- 2.cpp
+++ 2.cpp
@@ -11,10 +11,12 @@ int main(){
     cin >> v;
     cout << "Enter a: ";
     cin >> a;
-    t = v * sin(a) / g * (1 + sqrt(1 + (2 * g * h) / (pow(v, 2) * pow(sin(a), 2))));
-    x = v * t * cos(a);
-    y = h + v * t * sin(a) - 0.5 * g * pow(t, 2);
-    cout << "Result x: " << x << endl;
-    cout << "Result y: " << y << endl;
+    if((1 + (2 * g * h) / (pow(v, 2) * pow(sin(a), 2))) >= 0){
+        t = v * sin(a) / g * (1 + sqrt(1 + (2 * g * h) / (pow(v, 2) * pow(sin(a), 2))));
+        x = v * t * cos(a);
+        y = h + v * t * sin(a) - 0.5 * g * pow(t, 2);
+        cout << "Result x: " << x << endl;
+        cout << "Result y: " << y << endl;
+    }
     return 0;
 }
