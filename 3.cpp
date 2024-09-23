8ce3dd323e9b10aff0fa1acad3c33ae33b84d94b
Maria <mariya.Kirichek2006@mail.ru>
2024-09-23 13:34:17 +0300

    Add files via upload

--- 3.cpp
+++ 3.cpp
@@ -10,7 +10,7 @@ int main(){
     if(x > - 4 && x < -1){
         g = abs(x + 6) / (abs(x+3) - 3);
     }else if(x == -1.5){
-        g = sqrt(5 - x - 2);
+        g = sqrt(5 - x) - 2;
     }else if(x >= 5){
         g = log(x + 10) - 2;
     }else{
